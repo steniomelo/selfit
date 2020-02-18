c5843b86bb5a082a1c63fdb5f73e42ca9971c5ca
steniomelo <stenio@steniomelo.com.br>
2020-02-18 07:01:47 -0300

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
-  serverUrl: 'https://steniomelo.com.br/selfit',
+  serverUrl: 'https://selfitportal.herokuapp.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
