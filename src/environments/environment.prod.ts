70e293dabb05bf945da419add882a28a93cca6bb
steniomelo <stenio@steniomelo.com.br>
2020-03-03 10:17:36 -0300

    Atualizando enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,5 +13,12 @@ export const environment = {
   serverUrl: 'https://selfitportal.herokuapp.com/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR'],
-  chave: 'producao'
+  chave: 'producao',
+  API_PROSELF_UNI: 'http://treino-union.proselfit.com.br:8080',
+  API_PROSELF: 'http://lb-zw-web-939419841.sa-east-1.elb.amazonaws.com/api',
+  API_REDBOX_PROD: 'https://treino-backend.proselfit.com.br',
+  API_SITE: 'https://backend-selfitacademias.selfitacademias.com.br',
+  API_RED_PAYMENT: 'http://red-payments-dev.sa-east-1.elasticbeanstalk.com',
+  API_FOTOS: 'http://18.229.174.173:3001',
+  AUTH_CAPPTA: '669CAB98F33D4F70B768217457313582'
 };
